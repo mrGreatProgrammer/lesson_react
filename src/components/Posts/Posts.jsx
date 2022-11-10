ff23f077a566c40747a0d9d154a2bc69c4d3fdfa
mrGreateProgrammer <fhgf9048@gmail.com>
2022-11-10 20:35:28 +0500

    posts notfound

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,49 +1,55 @@
-import React from 'react'
-import { ThreeCircleLoader } from '../../imgs/Loadings';
-import { useGetPostsByLimitQuery } from '../../services/posts';
-import PaginationBtns from '../ui/PaginationBtns/PaginationBtns';
-import Post from './Post/Post'
+import React from "react";
+import { ThreeCircleLoader } from "../../imgs/Loadings";
+import { useGetPostsByLimitQuery } from "../../services/posts";
+import PaginationBtns from "../ui/PaginationBtns/PaginationBtns";
+import Post from "./Post/Post";
 
 const Posts = () => {
-    const [page, setPage] = React.useState(1)
-    const { data, error, isLoading, refetch, isFetching } = useGetPostsByLimitQuery(page);
-    
-    console.log(data);
-    
-    function toPrevPage(){
-        setPage(page=>page-1);
-        refetch()
-    }
-    function toNextPage(){
-        setPage(page=>page+1);
-        refetch();
-    }
+  const [page, setPage] = React.useState(1);
+  const { data, error, isLoading, refetch, isFetching } =
+    useGetPostsByLimitQuery(page);
+
+  console.log(data);
+
+  function toPrevPage() {
+    setPage((page) => page - 1);
+    refetch();
+  }
+  function toNextPage() {
+    setPage((page) => page + 1);
+    refetch();
+  }
 
   return (
-    <div className={"posts"} >
-        <h1>Posts</h1>
-        <div className="posts__container" >
-
+    <div className={"posts"}>
+      <h1>Posts</h1>
+      <div className="posts__container">
         {error ? (
-            <>Oh no, there was an error</>
-            ) : isFetching ? (
-                <ThreeCircleLoader width={"75"} height={"75"} />
-                ) : (data.map((e,i)=><Post 
+          <>Oh no, there was an error</>
+        ) : isFetching ? (
+          <ThreeCircleLoader width={"75"} height={"75"} />
+        ) : data === null || !data?.length ? (
+            <div className="notFound__msg">not found</div>
+            ) : (
+            data.map((e, i) => (
+              <Post
                 postId={e.id}
                 userId={e.userId}
                 title={e.title}
                 postBody={e.body}
                 key={i}
-                />))}
-                </div>
+              />
+            ))
+        )}
+      </div>
 
-        <PaginationBtns 
+      <PaginationBtns
         currentPage={page}
         toPrevPage={toPrevPage}
         toNextPage={toNextPage}
-        />
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
--- src/services/posts.js
+++ src/services/posts.js
@@ -6,7 +6,7 @@ export const postsApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
   endpoints: (builder) => ({
     getPostsByLimit: builder.query({
-      query: (page) => `posts?_limit=5&_page=${page}`,
+      query: (page) => `posts?_limit=25&_page=${page}`,
     }),
     getCommentsByPostId: builder.query({
       query: (postId) => `posts/${postId}/comments`,
