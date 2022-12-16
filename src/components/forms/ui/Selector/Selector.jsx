93abb8cbeb32a3dfaa63699ab807fe95234b5aeb
mrGreateProgrammer <fhgf9048@gmail.com>
2022-12-16 19:13:49 +0500

    selector

--- src/components/forms/ui/Selector/Selector.jsx
+++ src/components/forms/ui/Selector/Selector.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
+import Select from 'react-select';
 
-const Selector = ({dataOptions, name, selectorId, disabled, defaultValue, labelTxt, currentValue}) => {
-  // const [currentValueSelector, setCurrentValueSelector] = React.useState(null);
-
-  function selector(event) {
-    console.log("selector event >>>>>", event)
-    // setCurrentValueSelector("")
-  }
+const Selector = ({
+  dataOptions,
+  name,
+  selectorId,
+  disabled,
+  defaultValue,
+  labelTxt,
+  currentValue,
+  isLoading,
+  isClearable,
+  isSearchable
+}) => {
 
   return (
     <div>
-      <select name={name} id={selectorId} disabled={disabled} defaultValue={defaultValue} onChange={selector} onClick={(event)=>console.log("click event selector", event)}
-      
-      >
-        {dataOptions.map((e,id)=><option key={id} value={e.id} >{e.name}</option>)}
-      </select>
-      <label htmlFor={selectorId}>{labelTxt}</label>
+      {labelTxt&& <label htmlFor={name}>{labelTxt}</label> }
+       <Select
+        className="basic-single"
+        classNamePrefix="select"
+        defaultValue={defaultValue}
+        isDisabled={disabled}
+        isLoading={isLoading}
+        isClearable={isClearable}
+        isSearchable={isSearchable}
+        name={name}
+        options={dataOptions}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Selector
+export default Selector;
